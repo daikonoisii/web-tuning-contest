357e59fef7ad10282374f88d28e7d7985d725649
dishizawa <dishizawa2001@gmail.com>
2025-04-05 21:18:35 +0900

    fix: :fire: lighthouseから不要な機能を最駆除

--- lighthouse-flows-generator/src/commands/generate.ts
+++ lighthouse-flows-generator/src/commands/generate.ts
@@ -1,15 +1,10 @@
 import type { Arguments, CommandBuilder } from "yargs";
 import puppeteer from "puppeteer";
-import FlowResult from "lighthouse/types/lhr/flow";
-import Result from "lighthouse/types/lhr/lhr";
-import { readFileSync, writeFileSync } from "fs";
-// @ts-ignore
-import { generateFlowReportHtml } from "lighthouse/report/generator/report-generator";
+import { writeFileSync } from "fs";
 // @ts-ignore
 import { startFlow } from "lighthouse/lighthouse-core/fraggle-rock/api.js";
 
 type Options = {
-  files?: string[];
   urls?: string[];
 };
 
@@ -21,23 +16,10 @@ export const builder: CommandBuilder<Options, Options> = (yargs) =>
 
 export const handler = async (argv: Arguments<Options>): Promise<void> => {
   const name = new Date().toString();
-  if (argv.files && argv.files.length > 0) {
-    const flowResult: FlowResult = {
-      name,
-      steps: argv.files.map((file, index): FlowResult.Step => {
-        const lhr: Result = JSON.parse(readFileSync(file, "utf8"));
-        return {
-          name: index.toString(),
-          lhr,
-        };
-      }),
-    };
-    const report = generateFlowReportHtml(flowResult);
-    writeFileSync(`${name}.html`, report);
-  } else if (argv.urls && argv.urls.length > 0) {
+  if (argv.urls && argv.urls.length > 0) {
     const browser = await puppeteer.launch({
       headless: true,
-      timeout: 60000
+      timeout: 120000
     });
     const page = await browser.newPage();
 
