c118a2f059fd7fc5345e6e4440fceacb9ed1ed91
dishizawa <dishizawa2001@gmail.com>
2025-04-09 23:43:33 +0900

    feat :sparkles: lighthouse-flows-generatorのLambdaを実装

--- lighthouse-flows-generator/src/commands/generate.ts
+++ lighthouse-flows-generator/src/commands/generate.ts
@@ -1,38 +1,73 @@
-import type { Arguments, CommandBuilder } from "yargs";
-import puppeteer from "puppeteer";
-import { writeFileSync } from "fs";
+import chromium from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
+import { S3 } from "aws-sdk";
 // @ts-ignore
 import { startFlow } from "lighthouse/lighthouse-core/fraggle-rock/api.js";
 
-type Options = {
-  urls?: string[];
-};
+export const handler = async (event: any, context: any) => {
+  console.log("Event:", event);
 
-export const builder: CommandBuilder<Options, Options> = (yargs) =>
-  yargs.options({
-    files: { array: true, string: true },
-    urls: { array: true, string: true },
+  const body = JSON.parse(event.body || "{}");
+  const urls: string[] = body.urls;
+  const studentId: string = body.student_id
+
+  const s3 = new S3();
+
+  if (!urls || urls.length === 0) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "No URLs provided" }),
+    };
+  }
+
+  if (!studentId || studentId.length === 0) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "No student ID" }),
+    };
+  }
+
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    timeout: 120000,
   });
 
-export const handler = async (argv: Arguments<Options>): Promise<void> => {
-  const name = new Date().toString();
-  if (argv.urls && argv.urls.length > 0) {
-    const browser = await puppeteer.launch({
-      headless: true,
-      timeout: 120000
-    });
-    const page = await browser.newPage();
+  const page = await browser.newPage();
 
-    const flow = await startFlow(page, { name });
+  const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+  const name = `lighthouse-${timestamp}`;
+  const flow = await startFlow(page, { name });
 
-    for (const url of argv.urls) {
-      await flow.navigate(url)
-    }
-    
-    await browser.close();
-
-    const report = flow.generateReport();
-    writeFileSync(`${name}.html`, report);
+  for (const url of urls) {
+    await flow.navigate(url)
   }
-  process.exit(0);
+
+  const report = await flow.generateReport();
+  await browser.close();
+
+  const bucketName = process.env.S3_BUCKET_NAME;
+
+  if (!bucketName) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "S3_BUCKET_NAME is not defined in env" }),
+    };
+  }
+  await s3.putObject({
+    Bucket: bucketName,
+    Key: `${studentId}/${name}.html`,
+    Body: report,
+    ContentType: "text/html",
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Report uploaded to S3 successfully",
+      filename: `${studentId}/${name}.html`,
+    }),
+  };
 };
